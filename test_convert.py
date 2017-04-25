5fe6111f3b428c8944bcdbcfd798ad8dbc8f6ca7
jpRivera <juanpablo.rivera@marquette.edu>
2017-04-25 13:22:58 -0500

    Added changes to main, and convert

--- test_convert.py
+++ test_convert.py
@@ -1,15 +1,22 @@
-
 from PIL import Image
-
 
 
 def main():
 
 	print "Hello mars"
 
-	img = Image.open('eye_image_test1.jpg').convert('L')
-#	img.show()
-	img.save('eye_test.bin')
+	f = open('new_img.bin','w')
+	img = Image.open('eye_image_test1.jpg')
+	
+	#img.getPixel((0,0))
+
+	height = img.height
+	width = img.width
 
 
+	for i in height:
+		for j in width:
+			pixel=img.getPixel((i,j))
+			f.write(pixel + '\n')
+	f.close
 main()
